eee4f1b93d32fca33d4234f816a2612f2ba69536
hhf <huhangfeivip@163.com>
2026-05-17 19:42:57 +0800

    优化项目结构

--- hopaw-biz/src/main/java/com/agent/hopaw/biz/tool/websearch/WebSearchTool.java
+++ hopaw-biz/src/main/java/com/agent/hopaw/biz/tool/websearch/WebSearchTool.java
@@ -19,7 +19,7 @@ public class WebSearchTool implements AgentTool {
         this.qianfanWebSearchUtil = qianfanWebSearchUtil;
     }
 
-    @Tool(value={"搜索查询互联网最新网络信息，返回相关的网页标题和摘要内容。",""})
+    @Tool(value={"搜索查询互联网最新网络信息，返回相关的网页标题和摘要内容。","新闻、军事、财经、时事、天气、资料"})
     public String webSearch(@P(description = "搜索关键词") String query, @P(description = "搜索源（可选值有 baiduqianfan）默认 baiduqianfan", required = false) String source, @P(description = "最大结果数，默认5", required = false) Integer maxResults, @P(description = "超时时间（毫秒），默认10000毫秒", required = false) Integer timeout) {
         if (query == null || query.trim().isEmpty()) {
             return "错误: 搜索关键词不能为空";
