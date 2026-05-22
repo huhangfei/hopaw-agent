44d57666b4e7899770303d36befe1e53b3058642
hhf <huhangfeivip@163.com>
2026-05-22 11:22:35 +0800

    新增插件导出

---
+++ hopaw-contract/src/main/java/com/agent/hopaw/infra/model/dto/PluginExportInfo.java
@@ -0,0 +1,43 @@
+package com.agent.hopaw.infra.model.dto;
+
+import com.agent.hopaw.infra.constant.AgentToolSourceEnum;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonPropertyOrder({"name", "description", "keyword", "icon", "version", "author", "url",
+        "source", "jarFileName", "defaultUpdateUrl", "fileSize", "sha256Hash", "tools"})
+public class PluginExportInfo extends ToolSetInfo {
+
+    private long fileSize;
+    private String sha256Hash;
+
+    public PluginExportInfo(ToolSetInfo info, long fileSize, String sha256Hash) {
+        super(info.getName(), info.getDescription(), info.getIcon(), info.getTools(), info.getSource());
+        this.setVersion(info.getVersion());
+        this.setAuthor(info.getAuthor());
+        this.setUrl(info.getUrl());
+        this.setJarFileName(info.getJarFileName());
+        this.setKeyword(info.getKeyword());
+        this.setDefaultUpdateUrl(info.getDefaultUpdateUrl());
+        this.setHasConfigItems(info.isHasConfigItems());
+        this.fileSize = fileSize;
+        this.sha256Hash = sha256Hash;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getSha256Hash() {
+        return sha256Hash;
+    }
+
+    public void setSha256Hash(String sha256Hash) {
+        this.sha256Hash = sha256Hash;
+    }
+}
